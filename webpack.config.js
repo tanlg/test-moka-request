fb1028afc1a519695f91302390972898c70769a2
LongguangTan <tanlongguang@mokahr.com>
2020-08-20 18:09:19 +0800

    Release version v1.0.5

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path'); // eslint-disable-line no-undef
+
+const devMode = process.env.NODE_ENV !== 'production'; // eslint-disable-line no-undef
+
+module.exports = {
+  // eslint-disable-line no-undef
+  entry: {
+    index: path.resolve(__dirname, './src/index.js'), // eslint-disable-line no-undef
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'), // eslint-disable-line no-undef
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    globalObject: "typeof self !== 'undefined' ? self : this", // webpack的一个bug导致umd无法在node上使用, 见https://github.com/webpack/webpack/pull/8625
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: false,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+};
